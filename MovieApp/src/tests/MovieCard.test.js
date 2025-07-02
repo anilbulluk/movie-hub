a706b47b1b9ee2787fe6e00983fbb5ca297d7532
Anıl Yılmaz Bulluk <anilyilmazbulluk@gmail.com>
2025-07-02 06:37:14 +0300

    movie hub

--- MovieApp/src/tests/MovieCard.test.js
+++ MovieApp/src/tests/MovieCard.test.js
@@ -5,25 +5,25 @@ import MovieCard from '../src/components/MovieCard'
 describe('Örnek veri testi:    ', ()=>{
  it('Hata. veri görüntülenemiyor', ()=>{
   const movie = {
-   id: "4cf9e571-3e69-4ad4-9be6-638d1828ecb6",
-   movie_id: 12149,
-   original_title: "Frailty",
-   original_language: "en",
-   overview: "A man confesses to an FBI agent his family's story of how his religious fanatic father's visions lead to a series of murders to destroy supposed 'demons'.",
-   popularity: 16.58,
-   poster_path: "https://image.tmdb.org/t/p/original/hER3sVCZ9cZRBO0KNI7NbtiLacn.jpg",
-   backdrop_path: "https://image.tmdb.org/t/p/original/vORzDiWl5mxm6nuXQu9h5bhi6rb.jpg",
-   release_date: "Sat, 11/17/2001",
-   vote_average: 7,
-   vote_count: 1037,
-   adult: 0,
+            "id": "b84a67f9-bee9-4262-bf01-bf8a8db8761f",
+            "movie_id": 35320,
+            "original_title": "School of Life",
+            "original_language": "en",
+            "overview": "At Fallbrook Middle School, the annual student-elected Teacher of the Year award is held. And every year for the last 43 years Norman Warner or most fondly called Stormin' Norman Warner has won the award. Now that he has died, the burden of carrying the legacy falls into the hands of Matt Warner, the son of the late Norman Warner who has always lived in the shadow of his father.",
+            "popularity": 8.43,
+            "poster_path": "https://image.tmdb.org/t/p/original/6v3lhMJLKQEsIpQRM56pU0FMwzW.jpg",
+            "backdrop_path": "https://image.tmdb.org/t/p/original/pYuTVQCfjv2YvpZ1jjsFKvjx7fe.jpg",
+            "release_date": "Sat, 02/19/2005",
+            "vote_average": 6.4,
+            "vote_count": 126,
+            "adult": 0,
   }
 
   const { getByText } = render(
    <MovieCard movie={movie} onPress={()=>{}} />
   )
-  expect(getByText('Frailty')).toBeTruthy()
-expect(getByText('A man confesses to an FBI agent his family')).toBeTruthy()
+  expect(getByText('School of Life')).toBeTruthy()
+expect(getByText('At Fallbrook Middle School')).toBeTruthy()
 }
 )
 })
